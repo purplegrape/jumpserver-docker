71d29df77ef1425391286663a16dc313523db430
purplegrape <purplegrape4@gmail.com>
2024-12-04 15:21:20 +0800

    Update entrypoint.sh

--- web/entrypoint.sh
+++ web/entrypoint.sh
@@ -6,6 +6,14 @@ if [ -d /data/nginx ] ;then
   /usr/bin/rsync -aq --delete /data/nginx/ /etc/nginx/
 fi
 
+if [ ! -d /data/lina ] ;then
+  /usr/bin/rsync -aq --delete /opt/lina/ /data/lina/
+fi
+
+if [ ! -d /data/luna ] ;then
+  /usr/bin/rsync -aq --delete /opt/luna/ /data/luna/
+fi
+
 mkdir -p /data/logs
 
 exec $@
