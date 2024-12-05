0bda1db81b700bd6364a594b6e23ca4a355e712c
purplegrape <purplegrape4@gmail.com>
2024-12-05 14:35:32 +0800

    update

--- core/entrypoint.sh
+++ core/entrypoint.sh
@@ -2,13 +2,13 @@
 
 set -x
 
-if [ ! -d /data/jumpserver ] ;then
+if [ ! -d /data/jumpserver ];then
   /usr/bin/rsync -aq --delete /opt/jumpserver/ /data/jumpserver/
+  cd /data/jumpserver
+  . /opt/py3/bin/activate
+  rm -rf apps/locale/zh/LC_MESSAGES/django.mo apps/locale/en/LC_MESSAGES/django.mo
+  django-admin compilemessages
+  chown -R nobody:nobody /data/jumpserver
 fi
-
-. /opt/py3/bin/activate
-cd /data/jumpserver
-rm -rf apps/locale/zh/LC_MESSAGES/django.mo apps/locale/en/LC_MESSAGES/django.mo
-python3.11 apps/manage.py compilemessages
 
 exec $@
