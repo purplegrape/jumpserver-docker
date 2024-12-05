4d53a4c462b0b43112238753c49b27310eb3c3ec
purplegrape <purplegrape4@gmail.com>
2024-12-05 18:51:22 +0800

    Update entrypoint.sh

--- koko/entrypoint.sh
+++ koko/entrypoint.sh
@@ -7,10 +7,7 @@ if [ ! -d /data/koko ];then
 cp -a /opt/koko /data/koko
 cat >/data/koko/config.yml <<EOF
 CORE_HOST: http://127.0.0.1:8080
-BOOTSTRAP_TOKEN: <PleasgeChangeSameWithJumpserver>
-SHARE_ROOM_TYPE: redis
-REDIS_HOST: 127.0.0.1
-REDIS_PORT: 6379
+BOOTSTRAP_TOKEN: <PleaseChangeSameWithJumpserver>
 EOF
 
 fi
