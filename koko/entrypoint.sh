efb1cef023bef9013b8a95c3841810ca59fb2e8d
purplegrape <purplegrape4@gmail.com>
2024-12-05 18:31:22 +0800

    Update entrypoint.sh

--- koko/entrypoint.sh
+++ koko/entrypoint.sh
@@ -5,10 +5,9 @@ set -x
 if [ ! -d /data/koko ];then
 
 cp -a /opt/koko /data/koko
-BOOTSTRAP_TOKEN=$(head -c10 < /dev/urandom | base64 | tr -dc A-Za-z0-9 | head -c 48)
 cat >/data/koko/config.yml <<EOF
 CORE_HOST: http://127.0.0.1:8080
-BOOTSTRAP_TOKEN: $BOOTSTRAP_TOKEN
+BOOTSTRAP_TOKEN: <PleasgeChangeSameWithJumpserver>
 SHARE_ROOM_TYPE: redis
 REDIS_HOST: 127.0.0.1
 REDIS_PORT: 6379
