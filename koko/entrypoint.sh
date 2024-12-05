985ba209d6aa3e799823eec8a2a2c2514f503314
purplegrape <purplegrape4@gmail.com>
2024-12-05 18:12:16 +0800

    Update entrypoint.sh

--- koko/entrypoint.sh
+++ koko/entrypoint.sh
@@ -3,8 +3,16 @@
 set -x
 
 if [ ! -d /data/koko ];then
-  cp -a /opt/koko /data/koko
-  mv /data/koko/config_example.yml /data/koko/config.yml
+
+cp -a /opt/koko /data/koko
+mv /data/koko/config_example.yml /data/koko/config.yml
+
+BOOTSTRAP_TOKEN=$(head -c10 < /dev/urandom | base64 | tr -dc A-Za-z0-9 | head -c 48)
+cat >/data/koko/config.yml <<EOF
+CORE_HOST: http://127.0.0.1:8080
+BOOTSTRAP_TOKEN: $BOOTSTRAP_TOKEN
+EOF
+
 fi
 
 exec $@
