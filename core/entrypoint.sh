e587ebe62912951d16e348ef4be633e57557b1f3
purplegrape <purplegrape4@gmail.com>
2024-12-05 16:54:47 +0800

    update

--- core/entrypoint.sh
+++ core/entrypoint.sh
@@ -2,18 +2,12 @@
 
 set -x
 
-if [ ! -d /opt/py3 ];then
-    cd /opt
-    unzip -qn /opt/py3.zip
-fi
-
 if [ ! -d /data/jumpserver ];then
     unzip -qn /opt/jumpserver.zip
     cd /data/jumpserver
     . /opt/py3/bin/activate
     rm -rf apps/locale/zh/LC_MESSAGES/django.mo apps/locale/en/LC_MESSAGES/django.mo
     django-admin compilemessages
-    chown -R nobody:nobody /data/jumpserver
 fi
 
 exec $@
