667881dbbb95e55731f9c03fce462650ee182ead
purplegrape <purplegrape4@gmail.com>
2024-12-05 17:25:25 +0800

    Update entrypoint.sh

--- core/entrypoint.sh
+++ core/entrypoint.sh
@@ -3,11 +3,27 @@
 set -x
 
 if [ ! -d /data/jumpserver ];then
-    unzip -qn /opt/jumpserver.zip
-    cd /data/jumpserver
-    . /opt/py3/bin/activate
-    rm -rf apps/locale/zh/LC_MESSAGES/django.mo apps/locale/en/LC_MESSAGES/django.mo
-    django-admin compilemessages
-fi
+unzip -qn /opt/jumpserver.zip
+cd /data/jumpserver
+. /opt/py3/bin/activate
+rm -rf apps/locale/zh/LC_MESSAGES/django.mo apps/locale/en/LC_MESSAGES/django.mo
+django-admin compilemessages
 
+SECRET_KEY=$(head -c100 < /dev/urandom | base64 | tr -dc A-Za-z0-9 | head -c 48)
+BOOTSTRAP_TOKEN=$(head -c10 < /dev/urandom | base64 | tr -dc A-Za-z0-9 | head -c 48)
+
+cat > /data/jumpserver/config.yml <<EOF
+SECRET_KEY: $SECRET_KEY
+BOOTSTRAP_TOKEN: $BOOTSTRAP_TOKEN
+DB_ENGINE: sqlite3
+DB_NAME: /data/jms.db
+
+HTTP_BIND_HOST: 0.0.0.0
+HTTP_LISTEN_PORT: 8080
+WS_LISTEN_PORT: 8070
+REDIS_HOST: 127.0.0.1
+REDIS_PORT: 6379
+EOF
+
+fi
 exec $@
