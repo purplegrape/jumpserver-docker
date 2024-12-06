de18825cf092f613a160db424566e963538db589
purplegrape <purplegrape4@gmail.com>
2024-12-06 14:28:24 +0800

    update

--- core/entrypoint.sh
+++ core/entrypoint.sh
@@ -15,8 +15,13 @@ BOOTSTRAP_TOKEN=$(head -c10 < /dev/urandom | base64 | tr -dc A-Za-z0-9 | head -c
 cat > /data/jumpserver/config.yml <<EOF
 SECRET_KEY: $SECRET_KEY
 BOOTSTRAP_TOKEN: $BOOTSTRAP_TOKEN
-DB_ENGINE: sqlite3
-DB_NAME: /data/jms.db
+DB_ENGINE: mysql
+DB_HOST: 127.0.0.1
+DB_PORT: 3306
+DB_USER: root
+DB_PASSWORD:
+DB_NAME: jumpserver
+
 
 HTTP_BIND_HOST: 0.0.0.0
 HTTP_LISTEN_PORT: 8080
