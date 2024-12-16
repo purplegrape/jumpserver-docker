cae99fd50371b961026ea70a7279d40b3261fdaa
purplegrape <purplegrape4@gmail.com>
2024-12-16 09:38:41 +0800

    update

---
+++ all-in-one/entrypoint.sh
@@ -0,0 +1,15 @@
+#!/usr/bin/env bash
+
+set -x
+
+if [ -d /data/nginx ] ;then
+  /usr/bin/rsync -aq --delete /data/nginx/ /etc/nginx/
+fi
+
+if [ ! -d /data/webroot ] ;then
+  /usr/bin/rsync -aq --delete /opt/webroot/ /data/webroot/
+fi
+
+mkdir -p /data/logs
+
+exec $@
